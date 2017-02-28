fac34a4edd36707a04a913758985de81d214ac73
Jonathan Scialpi <scialpi27@gmail.com>
2017-02-28 14:08:53 -0500

    Create Loops.java
    https://www.hackerrank.com/challenges/java-loops-i

---
+++ Loops.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        
+        if(number >= 2 && number <= 20){
+        for(int i=1;i<11;i++){
+            int result = number*i;
+            System.out.println(number+" x "+i+" = "+result);
+            }
+        }
+    }
+}
