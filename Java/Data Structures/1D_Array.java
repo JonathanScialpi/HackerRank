6bb74540b879e6444693a8613792fb762dc01b59
Jonathan Scialpi <scialpi27@gmail.com>
2017-02-28 21:08:15 -0500

    Create 1D_Array.java

---
+++ Java/Data Structures/1D_Array.java
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/java-1d-array-introduction
+        // Declare array a here
+int[] a = new int[n];
+        for(int i = 0 ; i < n; i++){
+            int val = scan.nextInt();
+            a[i]=val;// Fill array a here
+        }
