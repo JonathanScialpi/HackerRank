c31549f3e77a8fba920f665514e66cd748b31583
Jonathan Scialpi <scialpi27@gmail.com>
2017-02-28 21:11:20 -0500

    Create PhoneBook.java

---
+++ Java/Data Structures/PhoneBook.java
@@ -0,0 +1,30 @@
+// https://www.hackerrank.com/challenges/phone-book
+//Complete this code or write your own from scratch
+import java.util.*;
+import java.io.*;
+
+class Solution{
+   public static void main(String []argh)
+   {
+      HashMap<String,Integer> myMap = new HashMap<String,Integer>();
+      Scanner in = new Scanner(System.in);
+      int n=in.nextInt();
+      in.nextLine();
+      for(int i=0;i<n;i++)
+      {
+         String name=in.nextLine();
+         int phone=in.nextInt();
+         in.nextLine();
+         myMap.put(name,phone);
+      }
+      while(in.hasNext())
+      {
+         String s=in.nextLine();
+          if(myMap.containsKey(s)){
+         System.out.println(s+"="+myMap.get(s));
+          }else{
+         System.out.println("Not found");
+          }
+      }
+   }
+}
