bb42c168a6cb24ddfb296f1ef3e48cab99ee8faf
Jonathan Scialpi <scialpi27@gmail.com>
2017-02-28 21:13:35 -0500

    Create Generics.java

---
+++ Java/Data Structures/Generics.java
@@ -0,0 +1,10 @@
+// https://www.hackerrank.com/challenges/java-generics
+class Printer
+{
+   //Write your code here
+  void printArray(Object[] test){
+      for(int x=0;x<test.length;x++){
+          System.out.println(test[x]);
+      }
+  }
+}
