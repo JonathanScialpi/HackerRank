8a2ac1fa33b9853c1199015f8c0abac76e98cc14
Jonathan Scialpi <scialpi27@gmail.com>
2017-02-28 20:59:09 -0500

    Create Day1_DataTypes.java

---
+++ 30_Days_Of_Code/Day1_DataTypes.java
@@ -0,0 +1,17 @@
+// https://www.hackerrank.com/challenges/30-data-types
+        /* Declare second integer, double, and String variables. */
+int i2 = i+scan.nextInt();
+double d2 =d+scan.nextDouble();
+String junk = scan.nextLine();
+String s2 = scan.nextLine();
+s2 = s.concat(s2);
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+        /* Print the sum of both integer variables on a new line. */
+System.out.println(i2);
+        /* Print the sum of the double variables on a new line. */
+		System.out.println(d2);
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+System.out.println(s2);
