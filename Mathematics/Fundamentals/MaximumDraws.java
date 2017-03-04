1f35845990c7c5ecb9825b873adaa49414034184
Jonathan Scialpi <scialpi27@gmail.com>
2017-03-04 14:21:06 -0500

    Create MaximumDraws.java

---
+++ Mathematics/Fundamentals/MaximumDraws.java
@@ -0,0 +1,19 @@
+// https://www.hackerrank.com/challenges/maximum-draws
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+        for(int x=0;x<n;x++){
+            int current = in.nextInt();
+           
+                System.out.println(current+1);
+            
+        }
+    }
+}
