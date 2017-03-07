31d76880f229ce03c7b9807ced171cee1b253270
Jonathan Scialpi <scialpi27@gmail.com>
2017-03-07 11:18:49 -0500

    Create Day27_Testing.java

---
+++ 30_Days_Of_Code/Day27_Testing.java
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/30-testing
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+         System.out.println("5"); System.out.println("4 " + "3"); 
+         System.out.println("-1 " + "0 " + "4 " + "2"); 
+         System.out.println("5 " + "3"); 
+         System.out.println("0 " + "-1 " + "-2 " + "1 " + "-4");
+         System.out.println("6 " + "3 "); 
+         System.out.println("4 " + "-1 " + "0 " + "3 " + "4 " + "5"); 
+         System.out.println("3 " + "2"); 
+         System.out.println("-1 " + "99 " + "0");
+         System.out.println("8 " + "3 "); 
+         System.out.println("0 " + "1 " + "2 " + "-1 " + "4 " + "2 " + "1 " + "4");
+    }
+}
