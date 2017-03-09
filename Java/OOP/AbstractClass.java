c094956759ee1ded14f06c1fb29f5a123bbc504a
Jonathan Scialpi <scialpi27@gmail.com>
2017-03-09 15:04:58 -0500

    Create AbstractClass.java

---
+++ Java/OOP/AbstractClass.java
@@ -0,0 +1,7 @@
+// https://www.hackerrank.com/challenges/java-abstract-class
+//Write MyBook class here
+class MyBook extends Book{
+    public void setTitle(String s){
+       this.title = s;
+    }
+}
