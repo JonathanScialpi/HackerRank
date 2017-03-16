49bfac845bf3ab9c0108cb3ee49b2fed23a91b14
Jonathan Scialpi <scialpi27@gmail.com>
2017-03-16 10:52:33 -0400

    Create InsertionSort.java

---
+++ Algorithms/Sorting/InsertionSort.java
@@ -0,0 +1,51 @@
+// https://www.hackerrank.com/challenges/insertionsort1
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void insertIntoSorted(int[] ar) {
+        int n = ar.length;
+        int insert = ar[n-1];
+        
+        for(int x=n-1;x>=0;x--){ // 2 2 4 5 6 7 8 9 10
+          if(x==0 && insert<=ar[0]){
+                  ar[x]=insert;
+                  printArray(ar);
+              }else if(insert<=ar[x-1]){              
+            
+              ar[x] = ar[x-1];  
+                printArray(ar);
+          
+            }else if(insert>ar[x-1] && insert<ar[x+1]){
+                ar[x]= insert;                 
+                printArray(ar);
+            }
+           }
+         }
+    
+    
+    
+/* Tail starts here */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int s = in.nextInt();
+        int[] ar = new int[s];
+        for(int i=0;i<s;i++){
+            ar[i]=in.nextInt(); 
+        }
+        insertIntoSorted(ar);
+    }
+    
+    
+    private static void printArray(int[] ar) {
+        for(int n: ar){
+            System.out.print(n+" ");
+        }
+        System.out.println("");
+    }
+}
+ 
